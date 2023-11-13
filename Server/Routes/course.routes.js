2049bbcfd039f51f951ed97a3f83c90e644b489e
anand-bits <h20220277@pilani.bits-pilani.ac.in>
2023-11-13 15:51:19 +0530

    Course Updation. course Upload working

--- Server/Routes/course.routes.js
+++ Server/Routes/course.routes.js
@@ -1,14 +1,25 @@
 import { Router } from "express";
-import { getAllCourses, getLectureByCourseId } from "../controller/course.controller.js";
+import { createCourse, getAllCourses, getLectureByCourseId, removeCourse, updateCourse } from "../controller/course.controller.js";
+import { isLoggedIn } from "../middleware/auth.middleware.js";
+import upload from "../middleware/multer.middleware.js";
 
-const router= Router();
+const router = Router();
 
-
-
-router
-  .route('/')
+// Routes for handling courses
+router.route('/')
   .get(getAllCourses)
+  .post(upload.single('thumbnail'), createCourse);
 
-  router
-  .route('/:id')
+// Route for updating a course
+router.route('/:id')
+  .put(updateCourse);
+
+// Route for deleting a course
+router.route('/:id')
+  .delete(removeCourse);
+
+// Route for getting lectures of a course
+router.route('/:id/lectures')
+  .get(isLoggedIn, getLectureByCourseId);
+
 export default router;
--- Server/controller/course.controller.js
+++ Server/controller/course.controller.js
@@ -1,4 +1,7 @@
 import courseModel from "../models/course.model.js";
+import AppError from "../utils/error.utils.js";
+import fs from "fs/promises"
+import cloudinary from "cloudinary"
 
 const getAllCourses = async (req, res, next) => {
   try {
@@ -16,30 +19,134 @@ const getAllCourses = async (req, res, next) => {
     });
   }
 };
-
 const getLectureByCourseId = async (req, res, next) => {
-  try {
-    // Add your code to get lecture by course ID here
+    try {
+        const { id } = req.params;
+        const cleanedId = id.trim(); // Trim any leading or trailing whitespace
+        const course = await courseModel.findById(cleanedId).select('lectures');
 
-    // Example:
-    // const courseId = req.params.courseId;
-    // const lectures = await courseModel.findById(courseId).select('lectures');
-
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'Lectures for the course',
-    //   lectures,
-    // });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: 'Error fetching lectures for the course',
-      error: error.message,
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: 'Lectures for the course',
+            lectures: course.lectures,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error fetching lectures for the course',
+            error: error.message,
+        });
+    }
 };
+
+
+const createCourse=async (req,res,next)=>
+{
+
+    const {title,description,category,createdBy}= req.body
+
+    if(!title ||  !description || !category || !createdBy)
+    {
+        return next(new AppError("All fields arre required"),400);
+
+    } 
+    try{
+    const course=await courseModel.create({
+        title,
+        description,
+        category,createdBy,
+        thumbnail:{
+            public_id:'dummy',
+            secure_url:'dummy'
+        }
+
+
+    });
+
+    if(!course)
+    {
+        return next(new AppError("Course Dont created"),500);
+
+    }
+
+    if(req.file)
+    {
+        const result= await cloudinary.v2.uploader.upload(req.file.path,{
+            folder:'Learning Management System'
+        });
+        console.log(JSON.stringify(result))
+
+        if(result)
+        {
+            course.thumbnail.public_id=result.public_id;
+            course.thumbnail.secure_url=result.secure_url;
+
+        }
+        fs.rm(`uploads/${req.file.filename}`);
+
+    }
+    
+await course.save();
+res.status(200).json({
+    success:true,
+    message:"Course is Created",
+    course
+})
+   
+}
+catch(e)
+{  
+     return next(new AppError(e.message,500));
+
+}
+
+}
+const updateCourse = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const cleanedId = id.trim(); // Trim any leading or trailing whitespace
+        const course = await courseModel.findByIdAndUpdate(
+            cleanedId,
+            {
+                $set: req.body
+            },
+            {
+                runValidators: true
+            }
+        );
+
+        if (!course) {
+            return next(new AppError("Course not found", 404));
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Course is updated",
+            course
+        });
+    } catch (e) {
+        return next(new AppError(e.message, 500));
+    }
+};
+
+
+
+const removeCourse=async(req,res,next)=>
+{
+
+    try{
+        const {id}=req.params;
+        const course= await courseModel.findById(id);
+        if(!course)
+        {
+            return next(new AppError("Course dosnt exit with this id", 500));
+        }
+
+    }
+}
 
 export {
   getAllCourses,
-  getLectureByCourseId
+  getLectureByCourseId,
+  createCourse,updateCourse,removeCourse
 };
